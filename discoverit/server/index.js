d31144b7f060e7148335e9d394b35c00ef22ae36
Maksym Tkachuk <s2m4f6@gmail.com>
2021-11-20 03:05:14 +0200

    Implemented places photos retrieval

--- discoverit/server/index.js
+++ discoverit/server/index.js
@@ -5,7 +5,6 @@ const path = require('path');
 const fs = require('fs');
 
 const nearbySearchUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
-const mapsImageUrl = 'https://maps.googleapis.com/maps/api/place/photo';
 const data = JSON.parse(fs.readFileSync('src/data/data.json', 'utf8'));
 
 var app = express();
@@ -26,21 +25,10 @@ app.get('/api/places', (req, res) => {
     });
 });
 
-app.get('/api/places-image', (req, res) => {
-    const query = url.parse(req.url,true).query;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    axios.get(`${mapsImageUrl}?photo_reference=${query.photo_reference}&key=${data.maps.key}`).then(result => {
-        res.send(result.data)
-        console.log(result)
-    }).catch(error => {
-        console.log(error.response)
-    });
-});
-
 process.on('uncaughtException', (err) => {
     console.log("Uncaught exception: " + err);
 });
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), () => {
     console.log('Listening on ' + app.get('port'));
 });
--- discoverit/src/mapsApi.js
+++ discoverit/src/mapsApi.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
+import data from './data/data.json'
 
-const url = 'http://localhost:3001'
+const placesPhotosUrl = 'https://maps.googleapis.com/maps/api/place/photo';
+const backendUrl = 'http://localhost:3001';
 
-export function getPlacePhoto(place) {
-    console.log('getPlacePhoto: ' + place);
-    if (place === undefined) return {then: (x) => {}};
-    return axios.get(`${url}/api/places-image?photo_reference=${place.photos[0].photo_reference}`);
+export function setPlacePhoto(imageId, place, maxWidth = 1600, maxHeight = 900) {
+    document.getElementById(imageId).src = `${placesPhotosUrl}?photo_reference=${place.photos[0].photo_reference}&key=${data.maps.key}&maxwidth=${maxWidth}&maxheight=${maxHeight}`
 }
 
 export function getPlaces(keyword, location, radius = 1500) {
-    return axios.get(`${url}/api/places?keyword=${keyword}&location=${location}&radius=${radius}`);
+    return axios.get(`${backendUrl}/api/places?keyword=${keyword}&location=${location}&radius=${radius}`);
 }
 
 export function getNextPlaces(count, places, setPlaces, nextIndex, setNextIndex) {
-    console.log(count + ' ' + nextIndex);
     if (nextIndex+count >= places.length) {
         setPlaces(places.slice(nextIndex));
         setNextIndex(places.length);
