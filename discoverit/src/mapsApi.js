254c122f8ab2262577ffc55358cc3c0e0af0db41
Maksym Tkachuk <s2m4f6@gmail.com>
2021-11-20 03:07:39 +0200

    Moved backendUrl to data.json

--- discoverit/src/mapsApi.js
+++ discoverit/src/mapsApi.js
@@ -2,14 +2,13 @@ import axios from 'axios';
 import data from './data/data.json'
 
 const placesPhotosUrl = 'https://maps.googleapis.com/maps/api/place/photo';
-const backendUrl = 'http://localhost:3001';
 
 export function setPlacePhoto(imageId, place, maxWidth = 1600, maxHeight = 900) {
     document.getElementById(imageId).src = `${placesPhotosUrl}?photo_reference=${place.photos[0].photo_reference}&key=${data.maps.key}&maxwidth=${maxWidth}&maxheight=${maxHeight}`
 }
 
 export function getPlaces(keyword, location, radius = 1500) {
-    return axios.get(`${backendUrl}/api/places?keyword=${keyword}&location=${location}&radius=${radius}`);
+    return axios.get(`${data.backendUrl}/api/places?keyword=${keyword}&location=${location}&radius=${radius}`);
 }
 
 export function getNextPlaces(count, places, setPlaces, nextIndex, setNextIndex) {
